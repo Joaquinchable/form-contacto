ce4c2c80fa3bf46b0b6d800a99d2392a5e601b8d
Joaquin Chable <joaquinchable9@gmail.com>
2020-07-07 06:46:36 -0500

    form prueba

---
+++ email.php
@@ -0,0 +1,28 @@
+<?php
+// correo al que enviaremos el manesaje 
+$destinatario = 'joaquincharo.developer@gmail.com';
+
+
+
+// declaramos los inputs
+$nombre = $_POST ['nombre'];
+$telefono = $_POST['telefono'];
+$email =  $_POST['email'];
+$mensaje = $_POST['mensaje'];
+
+// declaramos el mensaje 
+$asunto = "Contacto desde pagina web";
+
+$carta = "De: $nombre \n";
+$carta .= "$email \n";
+$carta .= "$telefono \n";
+$carta .= "$mensaje \n";
+
+// declaramos todo lo que enviaremos al correo
+mail($destinatario, $asunto, $header, $carta);
+// alertas de que el correo ese envio y el segundo echo para regresar a la pagina
+echo "<script>alert('correo enviado exitosamente')</script>";
+echo "<script>alert setTimeout(\"location.href='index.html'\",1000)</script>";
+
+
+?>
