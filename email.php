90759e415d69b4a3876699d6144450434fea7ddf
Joaquin Chable <joaquinchable9@gmail.com>
2020-07-07 22:06:35 -0500

    actulizando html5

--- email.php
+++ email.php
@@ -1,28 +1,26 @@
 <?php
-
-// correo al que enviaremos el manesaje 
-$destinatario = 'joaquincharo.developer@gmail.com';
-
-
-
-// declaramos los inputs
-$nombre = $_POST['nombre'];
-$telefono = $_POST['telefono'];
-$email = $_POST['email'];
-$mensaje = $_POST['mensaje'];
-
-// declaramos el mensaje 
-
-
-
-$header = "Contacto desde pagina web";
-$mensajeCompleto = $mensaje . "\nAtentamente: " . $nombre;
-$datos = "tel: " . $telefono ;
-// declaramos todo lo que enviaremos al correo
-mail($destinatario, $mensajeCompleto, $header, $datos);
-// alertas de que el correo ese envio y el segundo echo para regresar a la pagina
-echo "<script>alert('correo enviado exitosamente')</script>";
-echo "<script>alert setTimeout(\"location.href='index.html'\",1000)</script>";
-
-
+// Check for empty fields
+if(empty($_POST['name'])  		||
+   empty($_POST['email']) 		||
+   empty($_POST['phone']) 		||
+   empty($_POST['message'])	||
+   !filter_var($_POST['email'],FILTER_VALIDATE_EMAIL))
+   {
+	echo "No arguments Provided!";
+	return false;
+   }
+	
+$name = $_POST['name'];
+$email_address = $_POST['email'];
+$phone = $_POST['phone'];
+$message = $_POST['message'];
+	
+// Create the email and send the message
+$to = 'joaquincharo.developer@gmail.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
+$email_subject = "Website Contact Form:  $name";
+$email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
+$headers = "From: noreply@yourdomain.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
+$headers .= "Reply-To: $email_address";	
+mail($to,$email_subject,$email_body,$headers);
+return true;			
 ?>
